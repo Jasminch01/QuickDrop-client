f8b7a0cf8c439df3fca784a5241def6d4dafb943
jasmin <jasminchakma895@gmail.com>
2024-03-13 07:44:55 +0600

    banner coustmized

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -11,47 +11,20 @@ const Banner = ({ title, size }) => {
 
         <div className="absolute z-0 inset-0 bg-black opacity-70"></div>
 
-
-        <div className="absolute z-0 top-[55%] lg:top-[45%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center w-[90%] lg:w-full">
+        <div className="absolute z-0 mt-32 top-[50%] lg:top-[45%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center w-[90%] lg:w-full space-y-5">
           <p
-            className={`mb-5 text-4xl ${size} leading-[120px] font-bold text-white hidden md:block w-[80%] mx-auto`}
+            className={`text-6xl ${size} font-bold text-white hidden md:block w-[50%] mx-auto`}
           >
             {/* Your Ultimate Home Cleaning Partner */}
             {/* {title} */}
-            30+ Years Experiences in Courier Service
+            Experience Seamless Parcel Management with QuickDrop
           </p>
-          <h1 className="mb-5 text-5xl xsm:text-4xl font-bold text-white leading-[3.5rem] md:hidden w-full">
-            {/* Your Ultimate Home Cleaning Partner */}
-            {/* {title} */}
-          </h1>
-          {/* <div>
-            <div className="flex space-y-5 md:space-y-0 items-center font-bold bg-slate-400/20 py-6  justify-center flex-col lg:flex-row  gap-2 lg:gap-10 mt-10 xsm:mb-32">
-              <div className="flex gap-1 items-center justify-center">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/128/1161/1161388.png"
-                  className="w-14"
-                  alt=""
-                />
-                <p>Reliable Cleaners</p>
-              </div>
-              <div className="flex gap-1 items-center justify-center">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/128/9464/9464085.png"
-                  className="w-14 mx-2"
-                  alt=""
-                />
-                <p>Top-Rated Google Reviews</p>
-              </div>
-              <div className="flex items-center gap-1 justify-center">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/128/10809/10809834.png"
-                  className="w-14"
-                  alt=""
-                />
-                <p>100% Guaranteed Bond Back</p>
-              </div>
+          <div className="w-[30%] mx-auto">
+            <div className="space-x-5">
+              <button className="py-3 px-4 bg-blue-500 rounded-full">Learn more</button>
+              <button className="py-3 px-4 bg-blue-500 rounded-full">Contact Us</button>
             </div>
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
